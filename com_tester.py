6bfce98a960ccf8f29b0fb3ee2e3335c313a44d8
fyved24 <fyved24@163.com>
2021-10-07 16:49:20 +0800

    添加从文件读取数据发送到串口

---
+++ com_tester.py
@@ -0,0 +1,15 @@
+import serial
+import time
+
+file_path = 'C:\\Users\\pc\\Desktop\\Rec211007151936.txt'
+ser = serial.Serial('COM4', 115200, bytesize=8)
+with open(file_path, 'r') as file:
+    data = file.readline()
+    hex_list = data.split(' ')
+    print(hex_list)
+    for hex_code in hex_list:
+        byte_array = bytes.fromhex(hex_code)
+        print(byte_array)
+        ser.write(byte_array)
+        time.sleep(1)
+    ser.close()
